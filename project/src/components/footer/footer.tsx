bfc15e526cb5e3b41a518e9ca607899e7c9569d0
Ilia <ilytar@gmail.com>
2022-11-01 00:42:24 +0300

    исправляет ошибку с лого футера

--- project/src/components/footer/footer.tsx
+++ project/src/components/footer/footer.tsx
@@ -1,9 +1,16 @@
-import Logo from '../logo/logo';
+import {Link} from 'react-router-dom';
+import {AppRoute} from '../../const';
 
 function Footer(): JSX.Element {
   return (
     <footer className="page-footer">
-      <Logo />
+      <div className="logo">
+        <Link to={AppRoute.Main} className="logo__link logo__link--light">
+          <span className="logo__letter logo__letter--1">W</span>
+          <span className="logo__letter logo__letter--2">T</span>
+          <span className="logo__letter logo__letter--3">W</span>
+        </Link>
+      </div>
 
       <div className="copyright">
         <p>© 2019 What to watch Ltd.</p>
