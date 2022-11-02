afd7f27567ab7a5c546f5dd4405d25c1422cf15a
Ilia <ilytar@gmail.com>
2022-11-02 20:36:44 +0300

    Добавит моковые данные для 8 фильмов

---
+++ project/src/mocks/films.ts
@@ -0,0 +1,172 @@
+import {Films} from '../types/films';
+
+export const films: Films = [
+  {
+    id: 1,
+    name: 'The Grand Budapest Hotel',
+    posterImage: 'img/the-grand-budapest-hotel-poster.jpg',
+    previewImage: 'img/the-grand-budapest-hotel.jpg',
+    backgroundImage: 'img/bg-the-grand-budapest-hotel.jpg',
+    backgroundColor: '#ffffff',
+    videoLink: 'https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4',
+    previewVideoLink: 'https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4',
+    description: 'In the 1930s, the Grand Budapest Hotel is a popular European ski resort, presided over by concierge Gustave H. (Ralph Fiennes). Zero, a junior lobby boy, becomes Gustaves friend and protege.',
+    rating: 8.9,
+    scoresCount: 240,
+    director: 'Wes Anderson',
+    starring: [
+      'Bill Murray'
+    ],
+    runTime: 99,
+    genre: 'Comedy',
+    released: 2014,
+    isFavorite: false
+  },
+  {
+    id: 2,
+    name: 'Fantastic Beasts: The Crimes of Grindelwald',
+    posterImage: 'img/fantastic-beasts-the-crimes-of-grindelwald.jpg',
+    previewImage: 'img/fantastic-beasts-the-crimes-of-grindelwald.jpg',
+    backgroundImage: 'img/fantastic-beasts-the-crimes-of-grindelwald.jpg',
+    backgroundColor: '#ffffff',
+    videoLink: 'https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4',
+    previewVideoLink: 'https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4',
+    description: 'Fantastic Beasts',
+    rating: 8,
+    scoresCount: 242,
+    director: 'John Smith',
+    starring: [
+      'Dambldor'
+    ],
+    runTime: 123,
+    genre: 'Dramas',
+    released: 2015,
+    isFavorite: false
+  },
+  {
+    id: 3,
+    name: 'Aviator',
+    posterImage: 'img/aviator.jpg',
+    previewImage: 'img/aviator.jpg',
+    backgroundImage: 'img/aviator.jpg',
+    backgroundColor: '#ffffff',
+    videoLink: 'https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4',
+    previewVideoLink: 'https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4',
+    description: 'Aviator Leonardo',
+    rating: 9,
+    scoresCount: 250,
+    director: 'John Ivanov',
+    starring: [
+      'Leonardo'
+    ],
+    runTime: 113,
+    genre: 'Triller',
+    released: 2016,
+    isFavorite: false
+  },
+  {
+    id: 4,
+    name: 'We need to talk about Kevin',
+    posterImage: 'img/we-need-to-talk-about-kevin.jpg',
+    previewImage: 'img/we-need-to-talk-about-kevin.jpg',
+    backgroundImage: 'img/we-need-to-talk-about-kevin.jpg',
+    backgroundColor: '#ffffff',
+    videoLink: 'https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4',
+    previewVideoLink: 'https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4',
+    description: 'Need to talk',
+    rating: 7,
+    scoresCount: 212,
+    director: 'Petro Brosk',
+    starring: [
+      'Maks Korzh'
+    ],
+    runTime: 93,
+    genre: 'Romance',
+    released: 2017,
+    isFavorite: false
+  },
+  {
+    id: 5,
+    name: 'Johnny English',
+    posterImage: 'img/johnny-english.jpg',
+    previewImage: 'img/johnny-english.jpg',
+    backgroundImage: 'img/johnny-english.jpg',
+    backgroundColor: '#ffffff',
+    videoLink: 'https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4',
+    previewVideoLink: 'https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4',
+    description: 'Agent in England',
+    rating: 9,
+    scoresCount: 122,
+    director: 'Ryan',
+    starring: [
+      'Igor Antonov'
+    ],
+    runTime: 63,
+    genre: 'Kids & Family',
+    released: 2018,
+    isFavorite: false
+  },
+  {
+    id: 6,
+    name: 'Snatch',
+    posterImage: 'img/snatch.jpg',
+    previewImage: 'img/snatch.jpg',
+    backgroundImage: 'img/snatch.jpg',
+    backgroundColor: '#ffffff',
+    videoLink: 'https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4',
+    previewVideoLink: 'https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4',
+    description: 'this is description',
+    rating: 2,
+    scoresCount: 192,
+    director: 'Ekaterina Shulmann',
+    starring: [
+      'Dimitro'
+    ],
+    runTime: 103,
+    genre: 'Sci-Fi',
+    released: 2019,
+    isFavorite: false
+  },
+  {
+    id: 7,
+    name: 'Seven Years in Tibet',
+    posterImage: 'img/seven-years-in-tibet.jpg',
+    previewImage: 'img/seven-years-in-tibet.jpg',
+    backgroundImage: 'img/seven-years-in-tibet.jpg',
+    backgroundColor: '#ffffff',
+    videoLink: 'https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4',
+    previewVideoLink: 'https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4',
+    description: '7 years in the mountains',
+    rating: 5,
+    scoresCount: 212,
+    director: 'Catpain Jack',
+    starring: [
+      'Ilia Nishul'
+    ],
+    runTime: 83,
+    genre: 'Horror',
+    released: 2020,
+    isFavorite: false
+  },
+  {
+    id: 8,
+    name: 'War of the Worlds',
+    posterImage: 'img/war-of-the-worlds.jpg',
+    previewImage: 'img/war-of-the-worlds.jpg',
+    backgroundImage: 'img/war-of-the-worlds.jpg',
+    backgroundColor: '#ffffff',
+    videoLink: 'https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4',
+    previewVideoLink: 'https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4',
+    description: 'UFO is coming',
+    rating: 8,
+    scoresCount: 242,
+    director: 'Ferrari',
+    starring: [
+      'Ford'
+    ],
+    runTime: 123,
+    genre: 'Crime',
+    released: 2021,
+    isFavorite: false
+  },
+];
